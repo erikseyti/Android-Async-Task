a1b94ed9c0794054b500ebd366db10d82e6ff1f6
erikseyti <erikseyti@gmail.com>
2018-04-11 20:27:03 -0300

    Adicionado Funcionalidade na MainActivity para buscar uma imagem da Internet

--- app/src/main/java/com/example/aluno/asynctask/BuscarImagem.java
+++ app/src/main/java/com/example/aluno/asynctask/BuscarImagem.java
@@ -1,12 +1,26 @@
 package com.example.aluno.asynctask;
+import android.app.Activity;
+import android.drm.DrmStore;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by aluno on 11/04/18.
  */
 
 public class BuscarImagem extends AsyncTask<String,Integer,Bitmap> {
+
+    Activity activity;
+
+    public BuscarImagem(Activity activity) {
+        this.activity = activity;
+    }
 
     @Override
     protected void onPreExecute() {
@@ -16,11 +30,30 @@ public class BuscarImagem extends AsyncTask<String,Integer,Bitmap> {
     @Override
     protected void onPostExecute(Bitmap bitmap) {
         super.onPostExecute(bitmap);
+        ImageView imageView = (ImageView) activity.findViewById(R.id.imagemProjeto);
+        if (bitmap!=null)
+        {
+            imageView.setImageBitmap(bitmap);
+        }
     }
+
 
     @Override
     protected Bitmap doInBackground(String... strings) {
-        return null;
+
+        Bitmap imagem = null;
+
+        try {
+            URL url = new URL(strings[0]);
+            imagem = BitmapFactory.decodeStream(url.openStream());
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return imagem;
     }
 
     // recebe um array de String
--- app/src/main/java/com/example/aluno/asynctask/MainActivity.java
+++ app/src/main/java/com/example/aluno/asynctask/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BuscarImagem buscarImagem = new BuscarImagem(MainActivity.this);
+        buscarImagem.execute("http://www.blog.connectparts.com.br/wp-content/uploads/2017/06/o-tamanho-da-roda-pode-aumentar-o-consumo-de-combustivel-do-carro.jpeg");
     }
 }
